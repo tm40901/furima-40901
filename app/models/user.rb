dd504445f7bb34e36c284b537045b7163b36ebfb
tm40901 <daiki9583@gmail.com>
2024-06-04 17:15:40 +0900

    新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday,        presence: true
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々ー]+\z/.freeze
+  validates :last_name, :first_name, format: { with: VALID_NAME_REGEX }
+
+  VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :last_name_kana, :first_name_kana, format: { with: VALID_KATAKANA_REGEX }
 end
