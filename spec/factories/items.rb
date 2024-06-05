46c272e6c010991dd8ac02b79fff7e3741dd08f6
tm40901 <daiki9583@gmail.com>
2024-06-05 21:34:25 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name            {Faker::Commerce.product_name}
+    information     {Faker::Lorem.sentence}
+    category_id     {2}
+    status_id       {2}
+    shipping_fee_id {2}
+    prefecture_id   {2}
+    schedule_id     {2}
+    price           {Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
