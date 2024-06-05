97b3be0f87bcb2e41ac4275dc3082e11378dd22a
tm40901 <daiki9583@gmail.com>
2024-06-05 17:00:00 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, 
+                                 :information, 
+                                 :category_id, 
+                                 :status_id,
+                                 :shipping_fee_id,
+                                 :prefecture_id,
+                                 :schedule_id,
+                                 :price,
+                                 :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :status, :shipping_fee, :prefecture, :schedule
-  
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :schedule
+
   belongs_to :user
   has_one_attached :image
 
