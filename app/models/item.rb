a2ddf34820dfbf937d70854adb311d3ba9419204
tm40901 <daiki9583@gmail.com>
2024-06-05 15:07:44 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :name,            presence: true
+  validates :information,     presence: true 
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id,   presence: true
+  validates :schedule_id,     presence: true
+  validates :price,           presence: true
+  validates :image,           presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname,        presence: true
   validates :last_name,       presence: true 
   validates :first_name,      presence: true
@@ -19,6 +21,4 @@ class User < ApplicationRecord
 
   VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
   validates :last_name_kana, :first_name_kana, format: { with: VALID_KATAKANA_REGEX }
-
-  has_many :items
 end
