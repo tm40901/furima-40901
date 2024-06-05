6f3d06c31c3c9285be73de4fdfb325f3ac72faa0
tm40901 <daiki9583@gmail.com>
2024-06-05 15:25:49 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :shipping_fee, :prefecture, :schedule
+  
   belongs_to :user
   has_one_attached :image
 
   validates :name,            presence: true
   validates :information,     presence: true 
-  validates :category_id,     presence: true
-  validates :status_id,       presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id,   presence: true
-  validates :schedule_id,     presence: true
   validates :price,           presence: true
   validates :image,           presence: true
+
+  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, numericality: { other_than: 1, message: "can't be blank"} 
 end
